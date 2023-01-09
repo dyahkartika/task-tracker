7f0775005878d09744e7bec52024b2eff7e8e9e4
Dyah Kartika <dyahkartikad15@gmail.com>
2023-01-09 22:17:56 +0700

    add function on button etc

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,8 +12,63 @@ import {
   useToast,
 } from "@chakra-ui/react";
 import { FaTrash, FaCheck } from "react-icons/fa";
+import { nanoid } from "nanoid";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addTodo,
+  toComplete as toCompleteRedux,
+  deleteTodo,
+} from "../store/actions";
 
-const Home = (props) => {
+const Home = () => {
+  const [input, setInput] = useState("");
+  const [completed, setCompleted] = useState([]);
+  const [toggle, setToggle] = useState(false);
+  const todosRedux = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+  const toast = useToast();
+  function handleOnChangeInput(event) {
+    setInput(event.target.value);
+  }
+
+  function handleKeyPress(event) {
+    if (event.key === "Enter") {
+      handleAddTodo();
+    }
+  }
+
+  function handleAddTodo() {
+    if (input) {
+      let newTodo = {
+        id: nanoid(),
+        title: input,
+        completed: false,
+      };
+      dispatch(addTodo(newTodo));
+      setInput("");
+    } else {
+      toast({
+        title: "Please fill the blank form.",
+        status: "warning",
+        duration: 9000,
+        isClosable: true,
+        variant: "left-accent",
+      });
+    }
+  }
+
+  function handleDeleteTodo(id) {
+    dispatch(deleteTodo(id));
+  }
+
+  function toComplete(id) {
+    dispatch(toCompleteRedux(id));
+  }
+
+  useEffect(() => {
+    setCompleted([...todosRedux].filter((t) => t.completed));
+  }, [todosRedux]);
+
   return (
     <Box
       w={{ base: "80%", md: "60%", lg: "60%" }}
@@ -31,7 +86,7 @@ const Home = (props) => {
           textAlign="center"
           mt="4"
         >
-          Task Tracker nya
+          Task Tracker
         </Text>
         <Divider
           w="50%"
@@ -47,8 +102,11 @@ const Home = (props) => {
           placeholder="add task here ..."
           w="60%"
           mr="4"
+          onChange={(e) => handleOnChangeInput(e)}
+          onKeyPress={handleKeyPress}
+          value={input}
         />
-        <Button bgColor="orange.200" color="orange.500">
+        <Button bgColor="orange.200" color="orange.500" onClick={handleAddTodo}>
           Add
         </Button>
       </Flex>
@@ -60,28 +118,79 @@ const Home = (props) => {
         height="70%"
         overflowX="hidden"
       >
-        <HStack
-          bgColor={"orange.200"}
-          color={"orange.500"}
-          w="90%"
-          px={4}
-          py={2}
-          borderRadius="xl"
-          my="1"
-        >
-          <Text fontWeight="bold">{"dummy title"}</Text>
-          <Spacer />
-          <IconButton bgColor="white" icon={<FaCheck />} />
-          <IconButton bgColor="white" icon={<FaTrash />} />
-        </HStack>
+        {!todosRedux.length ? (
+          <Text color="orange.500">No Task</Text>) : toggle ? (
+          completed.map((t) => (
+            <HStack
+              bgColor={t.completed ? "green.200" : "orange.200"}
+              color={t.completed ? "green.500" : "orange.500"}
+              w="90%"
+              px={4}
+              py={2}
+              borderRadius="xl"
+              my="1"
+              key={t.id}
+            >
+              <Text fontWeight="bold">{t.title}</Text>
+              <Spacer />
+              <IconButton
+                bgColor="white"
+                icon={<FaCheck />}
+                onClick={() => toComplete(t.id)}
+              />
+              <IconButton
+                bgColor="white"
+                icon={<FaTrash />}
+                onClick={() => handleDeleteTodo(t.id)}
+              />
+            </HStack>
+          ))
+        ) : (
+          todosRedux.map((t) => (
+            <HStack
+              bgColor={t.completed ? "green.200" : "orange.200"}
+              color={t.completed ? "green.500" : "orange.500"}
+              w="90%"
+              px={4}
+              py={2}
+              borderRadius="xl"
+              my="1"
+              key={t.id}
+            >
+              <Text fontWeight="bold">{t.title}</Text>
+              <Spacer />
+              <IconButton
+                bgColor="white"
+                icon={<FaCheck />}
+                onClick={() => toComplete(t.id)}
+              />
+              <IconButton
+                bgColor="white"
+                icon={<FaTrash />}
+                onClick={() => handleDeleteTodo(t.id)}
+              />
+            </HStack>
+          ))
+        )}
       </Flex>
       <Flex justify="center">
-        <Button bgColor="green.200" color="green.600">
-          See Completed Only
-        </Button>
-        <Button bgColor="gray.200" color="gray.600">
-          See All Tasks
-        </Button>
+        {!toggle ? (
+          <Button
+            bgColor="green.200"
+            color="green.600"
+            onClick={() => setToggle(!toggle)}
+          >
+            See Completed Only
+          </Button>
+        ) : (
+          <Button
+            bgColor="gray.200"
+            color="gray.600"
+            onClick={() => setToggle(!toggle)}
+          >
+            See All Tasks
+          </Button>
+        )}
       </Flex>
     </Box>
   );
